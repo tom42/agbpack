b5c1ecee86b9f9da39ec30685d4f736ad14eb202
tom42 <tom42@users.noreply.github.com>
2024-07-03 22:28:52 +0200

    Improve VtgTesting.cmake: * Suppress warnings from find_package * Added some comments * Make catch_discover_tests available when using FetchContent

--- cmake/VtgTesting.cmake
+++ cmake/VtgTesting.cmake
@@ -3,8 +3,11 @@
 
 function(enable_testing_with_catch2 version)
   enable_testing()
-  find_package(Catch2 ${version})
 
+  # Try installed Catch2 package first.
+  find_package(Catch2 ${version} QUIET)
+
+  # If Catch2 package is not installed, get it using FetchContent.
   if(Catch2_FOUND)
     message(STATUS "Catch2 ${version} found")
   else()
@@ -16,6 +19,14 @@ function(enable_testing_with_catch2 version)
       GIT_TAG        v${version}
     )
     FetchContent_MakeAvailable(Catch2)
+
+    # Make Catch.cmake and CatchShardTests.cmake available.
+    list(APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/extras")
   endif()
+
+  include(CTest)
+
+  # Include Catch.cmake to get the catch_discover_tests function.
+  include(Catch)
 
 endfunction()
