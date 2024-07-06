2aa814a42f828d388406dc215d946659d6cfba59
tom42 <tom42@users.noreply.github.com>
2024-07-06 11:32:01 +0200

    Copy output from input file (which is still assumed to have a particular content)

--- src/agbpack/agbpack.cppm
+++ src/agbpack/agbpack.cppm
@@ -15,13 +15,23 @@ export class rle_decoder final
 public:
     // TODO: do we want to check what output points to? Or do we simply check that iterators point all to the same element type?
     //       do we restrict ourselves to byte/unsigned char/signed char/char?
+    // TODO: well we can't even assume that InputIterator has the same element type as output iterator:
+    //       ifstream::rdbuf() returns something that works with char, but if we'd like to decompress to e.g. vector<unsigned char>, then there you go. ouch.
     template <typename InputIterator, typename OutputIterator>
     void decode(InputIterator input, InputIterator eof, OutputIterator output)
     {
         // TODO: actually decode stuff from input stream
-        *output++ = 'a';
-        *output++ = 'b';
-        *output++ = 'c';
+        //       currently we assume a particular file with three literals at the end only, which we copy to output, after skipping the header and the one and only flag byte)
+        (void)input; // TODO: remove
+        (void)eof; // TODO: remove
+        ++input;
+        ++input;
+        ++input;
+        ++input;
+        ++input;
+        *output++ = *input++;
+        *output++ = *input++;
+        *output++ = *input++;
     }
 private:
 };
--- test/agbpack_test/rle_decoder_test.cpp
+++ test/agbpack_test/rle_decoder_test.cpp
@@ -14,21 +14,22 @@ namespace
     std::vector<unsigned char> decode_file()
     {
         std::ifstream f;
-        f.exceptions(std::ifstream::badbit | std::ifstream::failbit); // TODO: do we want eofbit too?
+        f.exceptions(std::ifstream::badbit | std::ifstream::failbit | std::ifstream::eofbit); // TODO: do we want eofbit too?
 
         // TODO: unhardcode path, one way or another.
         // TODO: letting streams throw exceptions is somewhat silly: if the file cannot be opened we tend to get rather useless error messages ("failbit set", "stream error", that sort)
         f.open("C:\\Users\\mathy\\Desktop\\work\\github\\agbpack\\test\\agbpack_test\\data\\rle.literals-only.txt.compressed", std::ios_base::binary);
 
+        std::istreambuf_iterator<char> input(f.rdbuf());
+        std::istreambuf_iterator<char> eof;
+
         // TODO: somehow, pass in data to decode (e.g. a file. Maybe name it decode_file, or rle_decode_file)
         // TODO: open file, pass stream stuff to decoder for decoding
         // TODO: decoder should take a back_inserter or something
         agbpack::rle_decoder decoder;
-        // TODO: pass real start to input (what we're doing below is not good and is only there to satisfy the compiler)
-        // TODO: pass real end of input
 
         std::vector<unsigned char> output;
-        decoder.decode(output.begin(), output.end(), std::back_inserter(output));
+        decoder.decode(input, eof, std::back_inserter(output));
         return output;
     }
 }
