a23cb36d0306b3141153426b150ac3090209acd9
tom42 <tom42@users.noreply.github.com>
2024-07-06 20:27:12 +0200

    Added prototype for reading a file.

--- test/agbpack_test/testdata.hpp
+++ test/agbpack_test/testdata.hpp
@@ -4,8 +4,13 @@
 #ifndef AGBPACK_TESTDATA_HPP_20240706
 #define AGBPACK_TESTDATA_HPP_20240706
 
+#include <string>
+#include <vector>
+
 namespace agbpack_test
 {
+
+std::vector<unsigned char> read_testdata_file(const std::string& basename);
 
 }
 
