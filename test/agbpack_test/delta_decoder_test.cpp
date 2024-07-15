3280f939131ff7672aaa2e5499070b42ead01b6e
tom42 <tom42@users.noreply.github.com>
2024-07-15 22:12:38 +0200

    Set up empty test case for delta_decoder.

---
+++ test/agbpack_test/delta_decoder_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("delta_decoder_test")
+{
+    SECTION("Valid input")
+    {
+    }
+
+    SECTION("Invalid input")
+    {
+    }
+}
