eb83ee8c514eb9897e59086a65ebfb9edf9b8f17
tom42 <tom42@users.noreply.github.com>
2024-07-16 21:46:24 +0200

    Renamed delta decoder test files.

--- test/agbpack_test/delta_decoder_test.cpp
+++ test/agbpack_test/delta_decoder_test.cpp
@@ -18,7 +18,7 @@ TEST_CASE("delta_decoder_test")
     {
         // TODO: figure out a very simple test case (e.g. just one byte, or word) and implement that. Zero length output would be pretty simple too.
         // TODO: also have delta.16.good-1-word.txt
-        string filename_part = GENERATE("delta.8.good-1-byte.txt");
+        string filename_part = GENERATE("delta.good.8.1-byte.txt");
         auto expected_data = agbpack_test::read_file(filename_part + ".decoded");
 
         auto decoded_data = agbpack_test::decode_file(decoder, filename_part + ".encoded");
@@ -28,11 +28,10 @@ TEST_CASE("delta_decoder_test")
 
     SECTION("Invalid input")
     {
-        // TODO: possibly swap options and good/bad tag
         // TODO: wrong compression options
         // ...
         auto encoded_file = GENERATE(
-            "delta.8.bad.eof-inside-header.txt.encoded",
+            "delta.bad.8.eof-inside-header.txt.encoded",
             "delta.bad.wrong-compression-type-in-header.txt.encoded");
 
         CHECK_THROWS_AS(agbpack_test::decode_file(decoder, encoded_file), agbpack::bad_encoded_data);
