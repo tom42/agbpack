d70abb4d845426eaf9e0b4bba414786126304f9b
tom42 <tom42@users.noreply.github.com>
2024-07-19 10:46:22 +0200

    delta_decoder: new test: missing padding at end of 8 bit stream (fails)

--- test/agbpack_test/delta_decoder_test.cpp
+++ test/agbpack_test/delta_decoder_test.cpp
@@ -39,6 +39,7 @@ TEST_CASE("delta_decoder_test")
             "delta.bad.wrong-compression-type-in-header.txt.encoded",
             "delta.bad.wrong-compression-options-in-header.txt.encoded",
             "delta.bad.8.eof-inside-stream.bin.encoded",
+            "delta.bad.8.missing-padding-at-end-of-data.txt.encoded",
             "delta.bad.16.eof-inside-stream.bin.encoded");
 
         CHECK_THROWS_AS(agbpack_test::decode_file(decoder, encoded_file), agbpack::bad_encoded_data);
