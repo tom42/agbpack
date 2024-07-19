5124b98308ad77b3ff2ef81152281bae9825e929
tom42 <tom42@users.noreply.github.com>
2024-07-19 11:56:06 +0200

    Set up empty test case for lzss decoder.

---
+++ test/agbpack_test/lzss_decoder_test.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+import agbpack;
+
+TEST_CASE("lzss_decoder_test")
+{
+}
