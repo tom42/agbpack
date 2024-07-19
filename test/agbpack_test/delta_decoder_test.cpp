7fe661cf251e5f14a1b4b8627255d90566ce8fef
tom42 <tom42@users.noreply.github.com>
2024-07-19 15:25:45 +0200

    Take note: rework test data.

--- test/agbpack_test/delta_decoder_test.cpp
+++ test/agbpack_test/delta_decoder_test.cpp
@@ -32,6 +32,11 @@ TEST_CASE("delta_decoder_test")
 
     SECTION("Invalid input")
     {
+        // TODO: align wrong/invalid type and invalid option tests with LZSS
+        //       * invalid type
+        //       * valid but unexpected type
+        //       * invalid options
+        //       TAKE CARE: these files should be otherwise valid zero length input files
         auto encoded_file = GENERATE(
             "delta.bad.eof-inside-header.txt.encoded",
             "delta.bad.wrong-compression-type-in-header.txt.encoded",
