12016efc1b95cea9d428385d24f105a0008fa9f5
tom42 <tom42@users.noreply.github.com>
2024-07-25 20:00:52 +0200

    lzss_decoder: add test file with maximum latch length

--- test/agbpack_test/lzss_decoder_test.cpp
+++ test/agbpack_test/lzss_decoder_test.cpp
@@ -47,8 +47,6 @@ TEST_CASE("lzss_decoder_test")
 
     SECTION("Valid input")
     {
-        // TODO: files with compression
-        //       * longest match (0xf => 0xf + 3 = 18 repeatet characters)
         // TODO: file with and without compressed runs
         string filename_part = GENERATE(
             "lzss.good.1-uncompressed-byte.txt",
@@ -59,7 +57,8 @@ TEST_CASE("lzss_decoder_test")
             "lzss.good.zero-length-file.txt",
             "lzss.good.reference-with-minimum-offset.txt",
             "lzss.good.reference-with-maximum-offset.txt",
-            "lzss.good.reference-with-minimum-match-length.txt");
+            "lzss.good.reference-with-minimum-match-length.txt",
+            "lzss.good.reference-with-maximum-match-length.txt");
         auto expected_data = read_file(filename_part + ".decoded");
 
         CHECK(decode_file(decoder, filename_part + ".encoded") == expected_data);
