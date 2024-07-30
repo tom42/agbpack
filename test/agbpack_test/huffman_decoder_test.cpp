9e90ec099aadfb6a7249afea713e1e2d072762f4
tom42 <tom42@users.noreply.github.com>
2024-07-30 07:18:30 +0200

    Set up empty test case for huffman_decoder.

---
+++ test/agbpack_test/huffman_decoder_test.cpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include "testdata.hpp"
+
+import agbpack;
+
+namespace agbpack_test
+{
+
+TEST_CASE("huffman_decoder_test")
+{
+}
+
+}
