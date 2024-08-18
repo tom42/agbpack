1ccd7d7c7118d55cfbd859e5e7736f0d28a6749b
tom42 <tom42@users.noreply.github.com>
2024-08-18 22:42:27 +0200

    Document how test files were obtained.

--- test/agbpack_test/huffman_decoder_test.cpp
+++ test/agbpack_test/huffman_decoder_test.cpp
@@ -20,12 +20,16 @@ TEST_CASE("huffman_decoder_test")
     SECTION("Valid input")
     {
         string filename_part = GENERATE(
-            // TODO: somehow find/construct a file with 0 bytes of input. Also document how that file came to be
             // Notes on test files:
             // * Files consisting of a single byte of input have been created using GValiente's gba-huff.h,
             //   which doesn't seem to be available on github anymore.
             //   The encoded data subsequently needed manual fixing, since the uncompressed size in the header
             //   was wrong (4 bytes when it should have been 1 byte)
+            // * Files consisting of zero bytes of input have been manually derived from files consisting
+            //   of a single byte of input, since no reference encoder was able to encode zero bytes of input.
+            //   * Since there is no uncompressed data, there is no bitstream, so it was removed manually.
+            //   * Tree size byte is 1 (the minimum size). There is a root node with two bogus child nodes,
+            //     each containing the symbol 0.
             "huffman.good.8.0-bytes.txt",
             "huffman.good.8.1-byte.txt",
             "huffman.good.8.2-bytes.txt",
