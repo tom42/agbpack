dca41d7a9d918d6878d860f5ab107e052c75020c
tom42 <tom42@users.noreply.github.com>
2024-08-21 17:06:42 +0200

    Decide for next huffman decoder test.

--- test/agbpack_test/huffman_decoder_test.cpp
+++ test/agbpack_test/huffman_decoder_test.cpp
@@ -57,7 +57,8 @@ TEST_CASE("huffman_decoder_test")
             "huffman.bad.invalid-compression-type-in-header.txt.encoded",
             "huffman.bad.valid-but-unexpected-compression-type-in-header.txt.encoded",
             "huffman.bad.invalid-compression-options-in-header.txt.encoded",
-            "huffman.bad.4.garbage-in-unused-bits-of-leaf-node.txt.encoded");
+            "huffman.bad.4.garbage-in-unused-bits-of-leaf-node.txt.encoded",
+            "huffman.bad.8.huffman-tree-access-past-end-of-tree.encoded");
 
         CHECK_THROWS_AS(decode_file(decoder, encoded_file), agbpack::bad_encoded_data);
     }
