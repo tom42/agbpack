113c87cbc14c77dd680587b0827615537185fa0a
tom42 <tom42@users.noreply.github.com>
2024-08-27 22:47:00 +0200

    Initial sketch of delta_encoder_test

---
+++ test/agbpack_test/delta_encoder_test.cpp
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_test
+{
+
+TEST_CASE("delta_encoder_test")
+{
+    SECTION("8 bit")
+    {
+        // TODO: zero bytes of input
+        // TODO: one byte of input
+        // TODO: many bytes of input
+    }
+
+    SECTION("16 bit")
+    {
+        // TODO: zero bytes of input
+        // TODO: one byte of input
+        // TODO: many bytes of input
+    }
+}
+
+}
