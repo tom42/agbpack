8123cdf6ac15e3d0f96a2a510d47fbd33a3f60aa
tom42 <tom42@users.noreply.github.com>
2024-08-27 22:49:06 +0200

    Sketch out delta_encoder_test some more.

--- test/agbpack_test/delta_encoder_test.cpp
+++ test/agbpack_test/delta_encoder_test.cpp
@@ -15,6 +15,14 @@ TEST_CASE("delta_encoder_test")
         // TODO: zero bytes of input
         // TODO: one byte of input
         // TODO: many bytes of input
+
+        // TODO: sketch out how encoding works
+        //       * set options
+        //       * call encode
+        // TODO: verify using the decoder
+        //       * The decoder must be able to decode
+        //       * The decoded data must be the same as the original data
+        agbpack::delta_encoder encoder;
     }
 
     SECTION("16 bit")
