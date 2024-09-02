e9e40f624151274e9e5625765ae6d6c9992a893b
tom42 <tom42@users.noreply.github.com>
2024-09-02 12:17:22 +0200

    Remove todo: delta dencoder test structure is fine.
    Also move setting of 8/16 bit option into act phase.
    Probably doesn't matter much, but like that it's more visible,
    close to the call to encode_file().

--- test/agbpack_test/delta_encoder_test.cpp
+++ test/agbpack_test/delta_encoder_test.cpp
@@ -21,21 +21,16 @@ TEST_CASE("delta_encoder_test")
     {
         // TODO: many bytes of input
 
-        // TODO: sketch out how encoding works
-        //       * set options
-        //       * call encode
         // TODO: verify using the decoder (orly?)
         //       * The decoder must be able to decode
         //       * The decoded data must be the same as the original data
-
-        encoder.options(agbpack::delta_options::delta8);
-
         string filename_part = GENERATE(
             "delta.good.8.zero-length-file.txt",
             "delta.good.8.one-byte.txt",
             "delta.good.8.sine.bin");
         auto expected_data = read_file(filename_part + ".encoded");
 
+        encoder.options(agbpack::delta_options::delta8);
         auto encoded_data = encode_file(encoder, filename_part + ".decoded");
 
         CHECK(encoded_data == expected_data);
@@ -46,13 +41,11 @@ TEST_CASE("delta_encoder_test")
         // TODO: one byte of input (orly? should it not be a word?)
         // TODO: many bytes of input
         // TODO: what if input is an odd number of bytes?
-
-        encoder.options(agbpack::delta_options::delta16);
-
         string filename_part = GENERATE(
             "delta.good.16.zero-length-file.txt");
         auto expected_data = read_file(filename_part + ".encoded");
 
+        encoder.options(agbpack::delta_options::delta16);
         auto encoded_data = encode_file(encoder, filename_part + ".decoded");
 
         CHECK(encoded_data == expected_data);
