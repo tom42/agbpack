db8f8f8d551010d27ae6571ed649b1990df3332f
tom42 <tom42@users.noreply.github.com>
2024-09-24 21:32:10 +0200

    Set up rle_encoder_test

--- src/agbpack/agbpack-rle.cppm
+++ src/agbpack/agbpack-rle.cppm
@@ -55,4 +55,9 @@ public:
     }
 };
 
+export class rle_encoder final
+{
+public:
+};
+
 }
---
+++ test/agbpack_test/rle_encoder_test.cpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_test
+{
+
+TEST_CASE("rle_encoder_test")
+{
+    agbpack::rle_encoder encoder;
+    (void)encoder; // TODO: remove this
+}
+
+}
