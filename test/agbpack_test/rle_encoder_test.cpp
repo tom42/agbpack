174048bc05d7f420fa19d05a24bda2c327d17928
tom42 <tom42@users.noreply.github.com>
2024-09-30 07:41:08 +0200

    Add ideas for test cases

--- test/agbpack_test/rle_encoder_test.cpp
+++ test/agbpack_test/rle_encoder_test.cpp
@@ -23,7 +23,12 @@ TEST_CASE("rle_encoder_test")
         //       * "compressed [byte(s)]" => "repeated [byte(s)]"
         //       * uncompressed => literal
         // TODO: see what else we want to test (same as for the decoder? otoh it is also given by the test first development of the encoder)
-        string filename_part = GENERATE("rle.good.zero-length-file.txt");
+        string filename_part = GENERATE(
+            "rle.good.zero-length-file.txt"
+            // TODO: 1 literal
+            // TODO: 2 literals
+            // TODO: multiple of max literals and then some, so that buffer flush is required inside loop
+            );
         auto expected_data = read_file(filename_part + ".encoded");
 
         auto encoded_data = encode_file(encoder, filename_part + ".decoded");
