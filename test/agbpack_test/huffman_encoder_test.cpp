3db8ef2e64ec8694ffaad6cfc5dfb683a5c27ffc
tom42 <tom42@users.noreply.github.com>
2024-10-05 18:42:43 +0200

    Set up skeleton for huffman_encoder_test [no ci]

---
+++ test/agbpack_test/huffman_encoder_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_test
+{
+
+TEST_CASE("huffman_encoder_test")
+{
+}
+
+}
