523a13ead57cb458319b99ab29201ab76a91d1d6
tom42 <tom42@users.noreply.github.com>
2024-10-06 12:16:34 +0200

    Add decode_vector, required for huffman_decoder testing.
    Also express decode_file in terms of decode_vector [no ci]

--- test/agbpack_test/testdata.hpp
+++ test/agbpack_test/testdata.hpp
@@ -21,12 +21,17 @@ std::string get_testfile_path(const std::string& basename);
 const std::vector<unsigned char> read_file(const std::string& basename);
 
 template <typename TDecoder>
-const std::vector<unsigned char> decode_file(TDecoder& decoder, const std::string& basename)
+const std::vector<unsigned char> decode_vector(TDecoder& decoder, const std::vector<unsigned char>& input)
 {
-    std::vector<unsigned char> input = read_file(basename);
     std::vector<unsigned char> output;
     decoder.decode(input.begin(), input.end(), back_inserter(output));
     return output;
+}
+
+template <typename TDecoder>
+const std::vector<unsigned char> decode_file(TDecoder& decoder, const std::string& basename)
+{
+    return decode_vector(decoder, read_file(basename));
 }
 
 template <typename TEncoder>
