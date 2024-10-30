8d615d79eab00ce84302e3ef25a3b458cd9177d7
tom42 <tom42@users.noreply.github.com>
2024-10-30 22:19:20 +0100

    Remove unused stuff from testdata.hpp [no ci]

--- test/agbpack_test/testdata.hpp
+++ test/agbpack_test/testdata.hpp
@@ -17,8 +17,6 @@ std::size_t get_file_size(const std::string& path);
 
 std::ifstream open_binary_file(const std::string& path);
 
-std::string get_testfile_path(const std::string& basename);
-
 std::vector<unsigned char> read_file(const std::string& basename);
 
 template <typename TDecoder>
@@ -29,12 +27,6 @@ std::vector<unsigned char> decode_vector(TDecoder& decoder, const std::vector<un
     return output;
 }
 
-template <typename TDecoder>
-std::vector<unsigned char> decode_file(TDecoder& decoder, const std::string& basename)
-{
-    return decode_vector(decoder, read_file(basename));
-}
-
 template <typename TEncoder>
 std::vector<unsigned char> encode_vector(TEncoder& encoder, const std::vector<unsigned char>& input)
 {
@@ -43,13 +35,6 @@ std::vector<unsigned char> encode_vector(TEncoder& encoder, const std::vector<un
     return output;
 }
 
-template <typename TEncoder>
-std::vector<unsigned char> encode_file(TEncoder& encoder, const std::string& basename)
-{
-    return encode_vector(encoder, read_file(basename));
-}
-
-// TODO: if this works out, rewrite all tests to use this class and sort test files into subdirectories
 // TODO: have nothing here but IO functions (read file, get path)
 class test_data_directory final
 {
