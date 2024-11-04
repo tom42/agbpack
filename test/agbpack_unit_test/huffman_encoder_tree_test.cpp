0fc306dec1bd9e7c755ee16b334fb99cbc2d9ff0
tom42 <tom42@users.noreply.github.com>
2024-11-04 08:12:36 +0100

    Add empty test for new huffman_encoder_tree [no ci]

---
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace agbpack_unit_test
+{
+
+// TODO: tests
+//       * No symbols
+//       * One symbol
+//       * Two symbols
+//       * Tree with 256 symbols with same frequency
+//       * Maximum depth tree
+//       * Maximum handleable code length exceeded
+TEST_CASE("huffman_encoder_tree_test")
+{
+}
+
+}
