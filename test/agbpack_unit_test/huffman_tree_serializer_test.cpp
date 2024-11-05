418dff268b84e55d7a013b0c25c7838aadd17e13
tom42 <tom42@users.noreply.github.com>
2024-11-05 23:27:14 +0100

    Set up blank test for huffman_tree_serializer [no ci]

---
+++ test/agbpack_unit_test/huffman_tree_serializer_test.cpp
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_unit_test
+{
+
+// TODO: tests
+//       * No symbols
+//       * One symbol
+//       * Two symbols
+//       * Tree with 256 symbols with same frequency
+//       * Maximum depth tree
+//       * Maximum handleable code length exceeded
+TEST_CASE("huffman_tree_serializer_test")
+{
+}
+
+}
