2f614c83ee4812e0e9e3861e48145c2bc18f7280
tom42 <tom42@users.noreply.github.com>
2024-11-06 07:23:24 +0100

    Set up first serializer test (no symbols)
    Fails, serialize() doesn't do anything yet
    [no ci]

--- test/agbpack_unit_test/huffman_tree_serializer_test.cpp
+++ test/agbpack_unit_test/huffman_tree_serializer_test.cpp
@@ -2,11 +2,29 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
 
 import agbpack;
 
 namespace agbpack_unit_test
 {
+
+using std::vector;
+using agbpack::frequency_table;
+using agbpack::huffman_encoder_tree;
+using agbpack::huffman_tree_serializer;
+
+namespace
+{
+
+auto create_and_serialize_tree(const frequency_table& frequencies)
+{
+    huffman_encoder_tree tree(8, frequencies); // TODO: obtain symbol size from frequency table?
+    huffman_tree_serializer serializer;
+    return serializer.serialize(tree);
+}
+
+}
 
 // TODO: tests
 //       * No symbols
@@ -19,10 +37,12 @@ TEST_CASE("huffman_tree_serializer_test")
 {
     SECTION("No symbols")
     {
-        // TODO: create appropriate frequency table
-        // TODO: create tree from that
-        // TODO: serialize tree
-        // TODO: verify serialized tree
+        frequency_table frequencies(8);
+        const vector<unsigned char> expected_serialized_tree = {42, 43, 44, 45};
+
+        auto serialized_tree = create_and_serialize_tree(frequencies);
+
+        CHECK(serialized_tree == expected_serialized_tree);
     }
 }
 
