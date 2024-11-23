ee556f9f50c814a63bdf07f32c4234a48002f30d
tom42 <tom42@users.noreply.github.com>
2024-11-23 10:20:51 +0100

    Complete node_queue_test [no ci]

--- test/agbpack_unit_test/node_queue_test.cpp
+++ test/agbpack_unit_test/node_queue_test.cpp
@@ -15,8 +15,15 @@ TEST_CASE("node_queue_test")
 {
     node_queue queue;
 
-    queue.push(nullptr); // TODO: not null
-    queue.pop();
+    queue.reserve(3);
+
+    queue.push(Node::make_leaf('a', 3));
+    queue.push(Node::make_leaf('b', 1));
+    queue.push(Node::make_leaf('c', 2));
+
+    CHECK(queue.pop()->frequency() == 1);
+    CHECK(queue.pop()->frequency() == 2);
+    CHECK(queue.pop()->frequency() == 3);
 }
 
 }
