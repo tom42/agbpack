b1fbf2fc5caec9e84fa67fc1c6873f4106a4575a
tom42 <tom42@users.noreply.github.com>
2024-11-24 18:38:33 +0100

    Sketching out test [no ci]

--- test/agbpack_unit_test/huffman_encoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -4,15 +4,38 @@
 #include <catch2/catch_test_macros.hpp>
 
 import agbpack;
+import agbpack_unit_test.utility;
 
 namespace agbpack_unit_test
 {
 
+using agbpack::code_table_entry;
+using agbpack::frequency_table;
+using agbpack::huffman_encoder_tree;
+using agbpack::max_code_length;
+using agbpack::symbol;
+
 TEST_CASE("huffman_encoder_tree_test")
 {
+    constexpr auto symbol_size = 8;
+    frequency_table frequencies(symbol_size);
+
     SECTION("Create code table with maximum code length")
     {
-        // TODO: create_code_table: maximum code length (32 bits)
+        // TODO: factor this out, we need it at least once more
+        auto s = lucas_sequence(max_code_length + 1);
+        for (symbol i = 0; i < s.size(); ++i)
+        {
+            frequencies.set_frequency(i, s[i]);
+        }
+
+        huffman_encoder_tree tree(symbol_size, frequencies);
+
+        auto code_table = tree.create_code_table();
+
+        // TODO: check all codes and lengths. Problem: how?
+        //CHECK(code_table[0] == code_table_entry(0, 32, 0b11111111111111111111111111111010));
+        //CHECK(code_table[1] == code_table_entry(1, 32, 0b11111111111111111111111111111011));
     }
 
     SECTION("Maximum code length exceeded")
