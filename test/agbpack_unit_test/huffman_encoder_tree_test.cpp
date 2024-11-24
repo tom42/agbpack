152c60405afa6fb7e27b36c00e695dd4fc1df435
tom42 <tom42@users.noreply.github.com>
2024-11-24 20:38:13 +0100

    Use StringMaker to convert code_table_entry to string for output by Catch2 [no ci]

--- test/agbpack_unit_test/huffman_encoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <format>
 #include <ostream>
+#include <string>
 
 import agbpack;
 import agbpack_unit_test.utility;
@@ -16,12 +18,19 @@ bool operator==(const agbpack::code_table_entry& /*a*/, const agbpack::code_tabl
     return false;
 }
 
-std::ostream& operator<<(std::ostream& os, const agbpack::code_table_entry& /*e*/)
-{
-    // TODO: output all components (symbol, code, code length)
-    os << "x";
-    return os;
 }
+
+namespace Catch
+{
+
+template <>
+struct StringMaker<agbpack::code_table_entry>
+{
+    static std::string convert(const agbpack::code_table_entry& e)
+    {
+        return std::format("s={}, c={}, l={}", e.s(), e.c(), e.l());
+    }
+};
 
 }
 
@@ -53,8 +62,8 @@ TEST_CASE("huffman_encoder_tree_test")
         auto code_table = tree.create_code_table();
 
         // TODO: check all codes and lengths. Problem: how?
-        CHECK(code_table[0] == code_table_entry(0, 32, 0b11111111111111111111111111111010));
-        CHECK(code_table[1] == code_table_entry(1, 32, 0b11111111111111111111111111111011));
+        CHECK(code_table[0] == code_table_entry(0, 0b11111111111111111111111111111010, 32));
+        CHECK(code_table[1] == code_table_entry(1, 0b11111111111111111111111111111011, 32));
     }
 
     SECTION("Maximum code length exceeded")
