c48f1fed1b93e8abf921f75aae7eb3eefe8e7737
tom42 <tom42@users.noreply.github.com>
2024-11-24 18:47:10 +0100

    WIP: huffman_encoder_tree test [no ci]

--- test/agbpack_unit_test/huffman_encoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -2,9 +2,28 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <ostream>
 
 import agbpack;
 import agbpack_unit_test.utility;
+
+namespace
+{
+
+bool operator==(const agbpack::code_table_entry& /*a*/, const agbpack::code_table_entry& /*b*/)
+{
+    // TODO: compare all components (symbol, code, code length)
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& os, const agbpack::code_table_entry& /*e*/)
+{
+    // TODO: output all components (symbol, code, code length)
+    os << "x";
+    return os;
+}
+
+}
 
 namespace agbpack_unit_test
 {
@@ -34,8 +53,8 @@ TEST_CASE("huffman_encoder_tree_test")
         auto code_table = tree.create_code_table();
 
         // TODO: check all codes and lengths. Problem: how?
-        //CHECK(code_table[0] == code_table_entry(0, 32, 0b11111111111111111111111111111010));
-        //CHECK(code_table[1] == code_table_entry(1, 32, 0b11111111111111111111111111111011));
+        CHECK(code_table[0] == code_table_entry(0, 32, 0b11111111111111111111111111111010));
+        CHECK(code_table[1] == code_table_entry(1, 32, 0b11111111111111111111111111111011));
     }
 
     SECTION("Maximum code length exceeded")
