7f75171449b6d072554a7b3817101dae95be26f7
tom42 <tom42@users.noreply.github.com>
2024-11-24 16:17:49 +0100

    Set up skeleton for huffman_encoder_tree_test [no ci]

---
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_unit_test
+{
+
+TEST_CASE("huffman_encoder_tree_test")
+{
+
+// TODO: create_code_table: maximum code length (32 bits)
+
+// TODO: create_code_table: maximum code length exceeded
+
+}
+
+}
