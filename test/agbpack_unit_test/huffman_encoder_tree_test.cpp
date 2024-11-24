b1c95a544c34a3888bfda25fd27e9f9499d5d233
tom42 <tom42@users.noreply.github.com>
2024-11-24 17:25:11 +0100

    Sketch out test sections [no ci]

--- test/agbpack_unit_test/huffman_encoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -10,11 +10,15 @@ namespace agbpack_unit_test
 
 TEST_CASE("huffman_encoder_tree_test")
 {
+    SECTION("Create code table with maximum code length")
+    {
+        // TODO: create_code_table: maximum code length (32 bits)
+    }
 
-// TODO: create_code_table: maximum code length (32 bits)
-
-// TODO: create_code_table: maximum code length exceeded
-
+    SECTION("Maximum code length exceeded")
+    {
+        // TODO: create_code_table: maximum code length exceeded
+    }
 }
 
 }
