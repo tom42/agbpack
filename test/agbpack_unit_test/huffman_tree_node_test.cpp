2c09589c17b1e440646e11b057380a5e1373a014
tom42 <tom42@users.noreply.github.com>
2024-12-02 21:59:18 +0100

    Make test build again. It breaks due to unique_ptr usage [no ci]

--- test/agbpack_unit_test/huffman_tree_node_test.cpp
+++ test/agbpack_unit_test/huffman_tree_node_test.cpp
@@ -8,39 +8,37 @@ import agbpack;
 namespace agbpack_unit_test
 {
 
-// TODO: uncomment and fix once we know how the tree_node class is going to be called (tree_node? node? ...?)
-/*
-using agbpack::tree_node;
+using agbpack::huffman_tree_node;
 
 TEST_CASE("tree_node_test")
 {
     SECTION("make_leaf")
     {
-        auto leaf_node = tree_node::make_leaf('A', 42);
+        auto leaf_node = huffman_tree_node::make_leaf('A', 42);
 
         CHECK(leaf_node->is_internal() == false);
         CHECK(leaf_node->child(0) == nullptr);
         CHECK(leaf_node->child(1) == nullptr);
-        CHECK(leaf_node->value() == 'A');
+        CHECK(leaf_node->val() == 'A');
         CHECK(leaf_node->frequency() == 42);
     }
 
     SECTION("make_internal")
     {
-        auto child0 = tree_node::make_leaf('B', 43);
-        auto child1 = tree_node::make_leaf('C', 44);
-        auto internal_node = tree_node::make_internal(child0, child1);
+        auto child0 = huffman_tree_node::make_leaf('B', 43);
+        auto child1 = huffman_tree_node::make_leaf('C', 44);
+        auto internal_node = huffman_tree_node::make_internal(std::move(child0), std::move(child1));
 
         CHECK(internal_node->is_internal() == true);
         CHECK(internal_node->child(0) == child0);
         CHECK(internal_node->child(1) == child1);
-        CHECK(internal_node->value() == 0);
+        CHECK(internal_node->val() == 0);
         CHECK(internal_node->frequency() == 87);
     }
 
     SECTION("num_nodes and num_leaves, leaf node")
     {
-        auto leaf_node = tree_node::make_leaf(0, 0);
+        auto leaf_node = huffman_tree_node::make_leaf(0, 0);
 
         CHECK(leaf_node->num_nodes() == 1);
         CHECK(leaf_node->num_leaves() == 1);
@@ -48,11 +46,11 @@ TEST_CASE("tree_node_test")
 
     SECTION("num_nodes and num_leaves, internal node")
     {
-        auto child0 = tree_node::make_leaf(0, 0);
-        auto child1 = tree_node::make_leaf(0, 0);
-        auto child2 = tree_node::make_leaf(0, 0);
-        auto internal_node0 = tree_node::make_internal(child0, child1);
-        auto internal_node1 = tree_node::make_internal(internal_node0, child2);
+        auto child0 = huffman_tree_node::make_leaf(0, 0);
+        auto child1 = huffman_tree_node::make_leaf(0, 0);
+        auto child2 = huffman_tree_node::make_leaf(0, 0);
+        auto internal_node0 = huffman_tree_node::make_internal(std::move(child0), std::move(child1));
+        auto internal_node1 = huffman_tree_node::make_internal(std::move(internal_node0), std::move(child2));
 
         CHECK(internal_node0->num_nodes() == 3);
         CHECK(internal_node1->num_nodes() == 5);
@@ -61,5 +59,5 @@ TEST_CASE("tree_node_test")
         CHECK(internal_node1->num_leaves() == 3);
     }
 }
-*/
+
 }
