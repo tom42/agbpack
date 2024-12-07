b076d47563e8cda8148e2dc7478412909b06427c
tom42 <tom42@users.noreply.github.com>
2024-12-07 09:41:46 +0100

    Sketch out setup of unit test [no ci]

--- test/agbpack_unit_test/bitstream_writer_test.cpp
+++ test/agbpack_unit_test/bitstream_writer_test.cpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <iterator>
+#include <vector>
 
 import agbpack;
 
@@ -10,6 +12,12 @@ namespace agbpack_unit_test
 
 TEST_CASE("bitstream_writer_test")
 {
+    std::vector<unsigned char> output;
+    agbpack::unbounded_byte_writer x(back_inserter(output)); // TODO: make temporary
+    agbpack::bitstream_writer y(x); // TODO: better name
+
+    x.write8(0);
+
     // TODO: implement and test new bitstream_writer
     CHECK(false);
 }
