00fec07d23b78f4de8c06c0b902c1068ae90ff64
tom42 <tom42@users.noreply.github.com>
2024-12-07 09:46:26 +0100

    Rename variables / remove todos [no ci]

--- test/agbpack_unit_test/bitstream_writer_test.cpp
+++ test/agbpack_unit_test/bitstream_writer_test.cpp
@@ -13,10 +13,8 @@ namespace agbpack_unit_test
 TEST_CASE("bitstream_writer_test")
 {
     std::vector<unsigned char> output;
-    agbpack::unbounded_byte_writer x(back_inserter(output)); // TODO: make temporary
-    agbpack::bitstream_writer y(x); // TODO: better name
-
-    x.write8(0);
+    agbpack::unbounded_byte_writer byte_writer(back_inserter(output));
+    agbpack::bitstream_writer bitstream_writer(byte_writer);
 
     // TODO: implement and test new bitstream_writer
     CHECK(false);
