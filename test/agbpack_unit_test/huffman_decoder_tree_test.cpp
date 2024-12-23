8789c5cf10ede0d106b31c4ff922d963818943ea
tom42 <tom42@users.noreply.github.com>
2024-12-23 21:25:12 +0100

    Flesh out test [skip ci]

--- test/agbpack_unit_test/huffman_decoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_decoder_tree_test.cpp
@@ -2,15 +2,27 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
+
+import agbpack;
 
 namespace agbpack_unit_test
 {
+
+using agbpack::huffman_decoder_tree;
 
 TEST_CASE("huffman_decoder_tree_test")
 {
     SECTION("create_code_table encounters garbage in unused bits of symbol")
     {
         // TODO: add test: create_decode_tree when there is garbage in a node
+        const auto symbol_size = 4;
+        std::vector<unsigned char> serialized_tree{ 0x00, 0x00, 0x00, 0x00}; // TODO: real content
+
+        // TODO: read/construct tree
+        huffman_decoder_tree tree(symbol_size, );
+
+        // TODO: call create_code_table, should fail because of garbage in high bits of tree
         FAIL();
     }
 }
