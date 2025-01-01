0dc4bb302c559e512d95eaf0c9edb2ff1d648e95
tom42 <tom42@users.noreply.github.com>
2025-01-01 20:41:19 +0100

    Add test for header::create with valid sizes

--- test/agbpack_unit_test/header_test.cpp
+++ test/agbpack_unit_test/header_test.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 import agbpack;
 
@@ -10,9 +11,17 @@ namespace agbpack_unit_test
 
 using agbpack::header;
 using agbpack::huffman_options;
+using agbpack::maximum_uncompressed_size;
 
 TEST_CASE("header_test")
 {
+    SECTION("create, valid size")
+    {
+        auto valid_size = GENERATE(0u, maximum_uncompressed_size);
+
+        CHECK(header::create(huffman_options::h8, valid_size).uncompressed_size() == valid_size);
+    }
+
     SECTION("to_uint32_t")
     {
         auto header = header::create(huffman_options::h8, 0x123456);
