bf67b47c0489d6f82e67503fe7fcd7b9f8e3579b
tom42 <tom42@users.noreply.github.com>
2025-01-02 21:20:36 +0100

    Add empty lzss_encoder_test

---
+++ test/agbpack_test/lzss_encoder_test.cpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace agbpack_test
+{
+
+TEST_CASE("lzss_encoder_test")
+{
+}
+
+}
