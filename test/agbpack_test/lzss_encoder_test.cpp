c45ddbaa4460a845283ab2266f1dc56a124d7f3d
tom42 <tom42@users.noreply.github.com>
2025-01-03 18:53:28 +0100

    WIP: lzss_encoder_test: use test_data_fixture

--- test/agbpack_test/lzss_encoder_test.cpp
+++ test/agbpack_test/lzss_encoder_test.cpp
@@ -2,16 +2,25 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include "testdata.hpp"
 
 import agbpack;
 
 namespace agbpack_test
 {
 
-TEST_CASE("lzss_encoder_test")
+TEST_CASE_METHOD(test_data_fixture, "lzss_encoder_test")
 {
     agbpack::lzss_encoder encoder;
-    // TODO: use standard fixture and set test input directory, I guess
+    agbpack::lzss_decoder decoder;
+    set_test_data_directory("lzss_encoder");
+
+    // TODO: remove
+    (void)encoder;(void)decoder;
+
+    // TODO: add tests: successful encoding: encode stuff and check against decoder
+    //       * First test: zero bytes of input, no? Where to get reference data from? Do we need reference data?
+    //         * No we don't, but maybe check whether e.g. CUE can deal with zero bytes of input
 }
 
 }
