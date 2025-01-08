c90c6dede2d02798b01e84d73879070382ce72bf
tom42 <tom42@users.noreply.github.com>
2025-01-08 17:21:15 +0100

    Set up match_finder_test (empty, needs some love) [skip ci]

---
+++ test/agbpack_unit_test/match_finder_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+
+namespace agbpack_unit_test
+{
+
+TEST_CASE("match_finder_test")
+{
+}
+
+}
