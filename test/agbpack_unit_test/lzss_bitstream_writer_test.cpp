471faf2eb3cf3598ff223da450dc85aa82513060
tom42 <tom42@users.noreply.github.com>
2025-01-09 20:03:18 +0100

    lzss_bitstream_writer_test: test writing literal bytes

--- test/agbpack_unit_test/lzss_bitstream_writer_test.cpp
+++ test/agbpack_unit_test/lzss_bitstream_writer_test.cpp
@@ -10,11 +10,35 @@ namespace agbpack_unit_test
 {
 
 using agbpack::lzss_bitstream_writer;
+using bitstream = std::vector<unsigned char>;
 
 TEST_CASE("lzss_bitstream_writer_test")
 {
-    std::vector<unsigned char> bitstream;
-    lzss_bitstream_writer writer(bitstream);
+    bitstream actual_bitstream;
+    lzss_bitstream_writer writer(actual_bitstream);
+
+    SECTION("Write literal bytes")
+    {
+        writer.write_literal(0x11);
+        writer.write_literal(0x22);
+        writer.write_literal(0x33);
+        writer.write_literal(0x44);
+        writer.write_literal(0x55);
+        writer.write_literal(0x66);
+        writer.write_literal(0x77);
+        writer.write_literal(0x88);
+        writer.write_literal(0x99);
+
+        bitstream expected_bitstream =
+        {
+            0x00,
+            0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+            0x00,
+            0x99
+        };
+
+        CHECK(actual_bitstream == expected_bitstream);
+    }
 }
 
 }
