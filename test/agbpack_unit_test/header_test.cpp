29f7f0da80d2e4a4892b200f32f5be600c6161b8
tom42 <tom42@users.noreply.github.com>
2025-01-20 21:36:27 +0100

    Fiix code to build for 32 bit targets (MSVC / x86, anyway)

--- test/agbpack_unit_test/header_test.cpp
+++ test/agbpack_unit_test/header_test.cpp
@@ -30,16 +30,17 @@ TEST_CASE("header_test")
 
     SECTION("create, invalid size")
     {
-        // TODO: this is not going to work on 32 bit targets. What to do?
-
-        // Ideally we would use size_t literal suffixes here ('uz').
-        // Unfortunately VS2022 does not have them yet.
-        std::size_t invalid_size = GENERATE(maximum_uncompressed_size + 1ull, 0x100000000ull);
-
         CHECK_THROWS_MATCHES(
-            header::create(huffman_options::h8, invalid_size),
+            header::create(huffman_options::h8, maximum_uncompressed_size + 1),
             encode_exception,
             Catch::Matchers::Message("data to encode is too big"));
+
+#if SIZE_MAX > 0xffffffff
+        CHECK_THROWS_MATCHES(
+            header::create(huffman_options::h8, 0x100000000),
+            encode_exception,
+            Catch::Matchers::Message("data to encode is too big"));
+#endif
     }
 
     SECTION("to_uint32_t")
