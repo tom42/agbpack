b5ff943bd94330207c219eef11f8b9e6b44a7c75
tom42 <tom42@users.noreply.github.com>
2025-01-21 18:53:07 +0100

    Add another test case (test file does not yet exist) [skip ci]

--- test/agbpack_test/lzss_decoder_test.cpp
+++ test/agbpack_test/lzss_decoder_test.cpp
@@ -94,8 +94,8 @@ TEST_CASE_METHOD(test_data_fixture, "lzss_decoder_test")
             pair("lzss.bad.valid-but-unexpected-compression-type-in-header.txt", "encoded data is corrupt"),
             pair("lzss.bad.invalid-compression-options-in-header.txt", "encoded data is corrupt"),
             pair("lzss.bad.missing-padding-at-end-of-data.txt", "encoded data is corrupt"),
-            pair("lzss.bad.reference-at-beginning-of-file.txt", "encoded data is corrupt: reference outside of sliding window"));
-            // TODO: have another test: sliding window is not empty, but offset still goes past its beginning
+            pair("lzss.bad.reference-at-beginning-of-file.txt", "encoded data is corrupt: reference outside of sliding window"),
+            pair("lzss.bad.reference-outside-of-non-empty-sliding-window.txt", "xyz"));
 
         CHECK_THROWS_MATCHES(
             decode_file(decoder, filename),
