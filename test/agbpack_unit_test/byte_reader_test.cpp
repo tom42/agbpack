530d1ae5379b2aaf3b3275f5f25695292ce91eef
tom42 <tom42@users.noreply.github.com>
2025-01-27 08:10:53 +0100

    Sketch out first test for reader [skip ci]

--- test/agbpack_unit_test/byte_reader_test.cpp
+++ test/agbpack_unit_test/byte_reader_test.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
 
 import agbpack;
 import agbpack_unit_testkit;
@@ -14,7 +15,18 @@ namespace agbpack_unit_test
 // TODO: add non-throwing version of read16 (or whatever API delta encoder uses)
 TEST_CASE("byte_reader_test")
 {
+    SECTION("Empty input")
+    {
+        // TODO: allow easy creation of readers
+        // TODO: make this test case: state after creation:
+        //       * Can deal with empty input
+        //       * Can deal with non-empty input
+        std::vector<unsigned char> input;
+        agbpack::byte_reader reader(begin(input), end(input));
 
+        CHECK(reader.eof() == true);
+        CHECK(reader.nbytes_read() == 0);
+    }
 }
 
 }
