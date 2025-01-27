c74bdc1b80e638a287ccd404c39ff5c504a6fbdc
tom42 <tom42@users.noreply.github.com>
2025-01-27 18:04:47 +0100

    Work on byte_reader unit test

--- test/agbpack_unit_test/byte_reader_test.cpp
+++ test/agbpack_unit_test/byte_reader_test.cpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <utility>
 #include <vector>
 
 import agbpack;
@@ -10,22 +12,35 @@ import agbpack_unit_testkit;
 namespace agbpack_unit_test
 {
 
+using byte_reader = agbpack::byte_reader;
+using byte_vector = std::vector<unsigned char>;
+using pair = std::pair;
+
 // TODO: write test for existing functionality of byte_reader
 // TODO: add non-throwing version of read8
 // TODO: add non-throwing version of read16 (or whatever API delta encoder uses)
 TEST_CASE("byte_reader_test")
 {
-    SECTION("Empty input")
+    SECTION("State after creation")
     {
-        // TODO: allow easy creation of readers
-        // TODO: make this test case: state after creation:
-        //       * Can deal with empty input
-        //       * Can deal with non-empty input
-        std::vector<unsigned char> input;
-        agbpack::byte_reader reader(begin(input), end(input));
+        const auto [input, expected_eof] = GENERATE(
+            pair(byte_vector{}, true),
+            pair(byte_vector{ 0x01, 0x02 }, false));
+        byte_reader reader(begin(input), end(input));
 
-        CHECK(reader.eof() == true);
+        CHECK(reader.eof() == expected_eof);
         CHECK(reader.nbytes_read() == 0);
+    }
+
+    SECTION("Read data until EOF is reached")
+    {
+        byte_vector input{ 0x01, 0x02 };
+        byte_reader reader(begin(input), end(input));
+
+        // TODO: read data. Check
+        //       * read byte
+        //       * eof
+        //       * nbytes_read
     }
 }
 
