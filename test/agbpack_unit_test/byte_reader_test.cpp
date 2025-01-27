1f705d109c708ca89663d1fe3d5aca498c2899bf
tom42 <tom42@users.noreply.github.com>
2025-01-27 07:43:07 +0100

    Set up skeleton for byte_reader_test [skip ci]

---
+++ test/agbpack_unit_test/byte_reader_test.cpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+import agbpack_unit_testkit;
+
+namespace agbpack_unit_test
+{
+
+// TODO: write test for existing functionality of byte_reader
+// TODO: add non-throwing version of read8
+// TODO: add non-throwing version of read16 (or whatever API delta encoder uses)
+TEST_CASE("byte_reader_test")
+{
+
+}
+
+}
