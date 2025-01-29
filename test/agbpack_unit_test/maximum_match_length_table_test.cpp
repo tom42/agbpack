6b45660e7b392b4e4e3e732265f0799767436d33
tom42 <tom42@users.noreply.github.com>
2025-01-30 00:27:07 +0100

    Add test for maximum match length table

---
+++ test/agbpack_unit_test/maximum_match_length_table_test.cpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import agbpack;
+import agbpack_unit_testkit;
+
+namespace agbpack_unit_test
+{
+
+using agbpack::maximum_match_length_table;
+
+TEST_CASE("maximum_match_length_table_test")
+{
+}
+
+}
