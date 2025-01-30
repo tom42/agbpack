54a9b269fbbdf7593b8e76a7b92b3c73ec7f31df
tom <tom@z2>
2025-01-30 08:22:19 +0100

    Sketch out how to proceed further with the optimal_lzss_encoder [skip ci]

--- test/agbpack_unit_test/maximum_match_length_table_test.cpp
+++ test/agbpack_unit_test/maximum_match_length_table_test.cpp
@@ -14,6 +14,12 @@ using agbpack::maximum_match_length_table;
 TEST_CASE("maximum_match_length_table_test")
 {
     // TODO: test something here
+    // TODO: screw that: we're not going to have a class for this: it feels silly, somehow
+    //       * For starters and to keep things simple, then, organize the encoder as such:
+    //         0) Read in the entire input (do we have a utility function for this?)
+    //         1) Call create_maximum_match_lengths()  <=  This is what we be testing here
+    //         2) Call create_chosen_match_lengths()
+    //         3) Call encode()
 }
 
 }
