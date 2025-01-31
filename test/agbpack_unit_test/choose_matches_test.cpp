ed66333b0df808b3b9e6b74a7a1554a083eb0449
tom <tom@z2>
2025-01-31 12:41:28 +0100

    choose_matches_test: first test with empty input passes

--- test/agbpack_unit_test/choose_matches_test.cpp
+++ test/agbpack_unit_test/choose_matches_test.cpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <string>
+#include <vector>
 
 import agbpack;
 import agbpack_unit_testkit;
@@ -9,12 +11,16 @@ import agbpack_unit_testkit;
 namespace agbpack_unit_test
 {
 
+using byte_vector = std::vector<unsigned char>;
+using m = agbpack::match;
+using mv = std::vector<agbpack::match>;
+
 namespace
 {
 
-auto call_choose_matches()
+auto call_choose_matches(const std::string& s)
 {
-    // TODO: have a way to supply input. Do we supply matches, or a string as input? Does it matter? (Do we simply check degenerate cases like empty input here?)
+    agbpack::find_longest_matches(byte_vector(begin(s), end(s)));
     return agbpack::choose_matches({});
 }
 
@@ -22,12 +28,11 @@ auto call_choose_matches()
 
 TEST_CASE("choose_matches_test")
 {
-    // TODO: input of length 0
     // TODO: input of length 1
     // TODO: input of length 2
     // TODO: arbitrary input
     // TODO: can we find some sort of input where the optimal parsing actually does help
-    call_choose_matches(); // TODO: assert something here
+    CHECK(call_choose_matches("") == mv{});
 }
 
 }
