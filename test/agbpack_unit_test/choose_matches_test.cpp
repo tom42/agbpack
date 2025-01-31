2d05488362c66f616d38606814195f1b98474ca5
tom <tom@z2>
2025-01-31 12:47:09 +0100

    choose_matches_test: test with input of length 1.
    Fails, because we currently hardcode the case of l=0 [skip ci]

--- test/agbpack_unit_test/choose_matches_test.cpp
+++ test/agbpack_unit_test/choose_matches_test.cpp
@@ -28,11 +28,11 @@ auto call_choose_matches(const std::string& s)
 
 TEST_CASE("choose_matches_test")
 {
-    // TODO: input of length 1
     // TODO: input of length 2
     // TODO: arbitrary input
     // TODO: can we find some sort of input where the optimal parsing actually does help
-    CHECK(call_choose_matches("") == mv{});
+    CHECK(call_choose_matches("")  == mv{});
+    CHECK(call_choose_matches("a") == mv{ m(1, 0) });
 }
 
 }
