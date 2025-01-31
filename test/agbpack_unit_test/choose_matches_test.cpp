ed66dfe2c3689f48dd1900efb9a9ae857dc27745
tom42 <tom42@users.noreply.github.com>
2025-01-31 14:40:40 +0100

    Add more tests

--- test/agbpack_unit_test/choose_matches_test.cpp
+++ test/agbpack_unit_test/choose_matches_test.cpp
@@ -28,11 +28,12 @@ auto call_choose_matches(const std::string& s)
 
 TEST_CASE("choose_matches_test")
 {
-    // TODO: input of length 2
+    // TODO: input of length 3 (aaa)
     // TODO: arbitrary input
     // TODO: can we find some sort of input where the optimal parsing actually does help
-    CHECK(call_choose_matches("")  == mv{});
-    CHECK(call_choose_matches("a") == mv{ m(1, 0) });
+    CHECK(call_choose_matches("")   == mv{});
+    CHECK(call_choose_matches("a")  == mv{ m(1, 0) });
+    CHECK(call_choose_matches("ab") == mv{ m(1, 0), m(1, 0) });
 }
 
 }
