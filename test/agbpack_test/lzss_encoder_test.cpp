e69660d4211dbc72f41cc784be68ffeef052f5ea
tom42 <tom42@users.noreply.github.com>
2025-02-01 14:29:58 +0100

    Rename variable, reformat test data

--- test/agbpack_test/lzss_encoder_test.cpp
+++ test/agbpack_test/lzss_encoder_test.cpp
@@ -28,18 +28,18 @@ class lzss_encoder_test_data_fixture : public test_data_fixture {};
 class test_parameters final
 {
 public:
-    test_parameters(const char* filename, std::size_t expected_encoded_size)
+    test_parameters(const char* filename, std::size_t expected_greedy_encoded_size)
         : m_filename(filename)
-        , m_expected_encoded_size(expected_encoded_size)
+        , m_expected_greedy_encoded_size(expected_greedy_encoded_size)
     {}
 
     const char* filename() const { return m_filename; }
 
-    std::size_t expected_encoded_size() const { return m_expected_encoded_size; }
+    std::size_t expected_encoded_size() const { return m_expected_greedy_encoded_size; }
 
 private:
     const char* m_filename;
-    std::size_t m_expected_encoded_size;
+    std::size_t m_expected_greedy_encoded_size;
 };
 
 using lzss_encoder_types = std::tuple<lzss_encoder, optimal_lzss_encoder>;
@@ -59,14 +59,14 @@ TEMPLATE_LIST_TEST_CASE_METHOD(
     SECTION("Successful encoding")
     {
         const auto parameters = GENERATE(
-            test_parameters("lzss.good.zero-length-file.txt", 4),
-            test_parameters("lzss.good.1-literal-byte.txt", 8),
-            test_parameters("lzss.good.3-literal-bytes.txt", 8),
-            test_parameters("lzss.good.8-literal-bytes.txt", 16),
-            test_parameters("lzss.good.9-literal-bytes.txt", 16),
-            test_parameters("lzss.good.minimum-match.txt", 12),
-            test_parameters("lzss.good.maximum-match.txt", 28),
-            test_parameters("lzss.good.delta.cppm", 1556));
+            test_parameters("lzss.good.zero-length-file.txt",  4),
+            test_parameters("lzss.good.1-literal-byte.txt",    8),
+            test_parameters("lzss.good.3-literal-bytes.txt",   8),
+            test_parameters("lzss.good.8-literal-bytes.txt",  16),
+            test_parameters("lzss.good.9-literal-bytes.txt",  16),
+            test_parameters("lzss.good.minimum-match.txt",    12),
+            test_parameters("lzss.good.maximum-match.txt",    28),
+            test_parameters("lzss.good.delta.cppm",         1556));
         INFO(std::format("Test parameters: {}", parameters.filename()));
         const auto original_data = this->read_decoded_file(parameters.filename());
 
