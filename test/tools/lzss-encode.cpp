ab5de1a93cf51f0703a9fead80e9aff9df902a62
tom42 <tom42@users.noreply.github.com>
2025-02-15 16:50:01 +0100

    Implementing lzss-encode

--- test/tools/lzss-encode.cpp
+++ test/tools/lzss-encode.cpp
@@ -19,16 +19,26 @@ struct command_line_options final
     string input_file;
 };
 
+command_line_options parse_command_line(int argc, char* argv[])
+{
+    // TODO: need 3/4 args here: mode, input file, output file
+    if (argc != 2)
+    {
+        throw std::runtime_error("wrong number of arguments");
+    }
+
+    return command_line_options{argv[1]};
 }
 
-int main(int /*argc*/, char* argv[])
+}
+
+int main(int argc, char* argv[])
 {
-    // TODO: implement LZSS encoding
     try
     {
-        // TODO: parse command line
+        auto options = parse_command_line(argc, argv);
         // TODO: open/read file
-        // TODO: encode file
+        // TODO: encode file (can start with optimal for starters)
         return EXIT_SUCCESS;
     }
     catch (const std::exception& e)
