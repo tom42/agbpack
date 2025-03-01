4de633d54cbe905f7d6d07e4e91d4d7025697c02
tom <tom@z2>
2025-03-01 11:59:03 +0100

    Set up unit test suite for argpppp

---
+++ test/argpppp_test/program_name_test.cpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace argpppp_test
+{
+
+TEST_CASE("program_name_test")
+{
+    FAIL("meh");
+}
+
+}
