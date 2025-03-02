47a2faa5a8825f10964e99747200f4626fed7b9c
tom42 <tom42@users.noreply.github.com>
2025-03-02 21:00:27 +0100

    Figured out how to access argp global variables so that it works for both real argp and also on windows for argp-standalone.

--- src/agbpacker/agbpacker.cpp
+++ src/agbpacker/agbpacker.cpp
@@ -12,11 +12,9 @@ import argpppp;
 // TODO: nope does not work => maybe we provide this inside argpppp, where we know whether glibc or argp-standalone is used?
 // TODO: OK: With glibc/linux we need to *define* global variables like so:
 // TODO: OK: if we use extern "C" like so, then it works. Question is then, do we want to abstract this in the library, or better not. Maybe better not? Seems to be fragile enough as it is?
-extern "C"
-{
-    const char* argp_program_version = "0.3";
-    const char* argp_program_bug_address = "/dev/null";
-}
+// TODO: supply the real version from the agbpack version number. How do we get at that? macro or do we compile it int agbpack?
+// TODO: can we write a test that ensures the version command is actually there? Well I suppose we can simply run "testprogram --version" and look at the exitcode?
+extern "C" const char* argp_program_version = "0.3";
 
 int main(int argc, char** argv)
 {
