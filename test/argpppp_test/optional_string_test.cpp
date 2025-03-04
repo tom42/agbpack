f04775767c9f7e1f2cc1d6907ee83909c2e50c33
tom <tom@z2>
2025-03-05 00:44:09 +0100

    Add test for optional_string

---
+++ test/argpppp_test/optional_string_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import argpppp;
+
+namespace argpppp_test
+{
+
+TEST_CASE("optional_string_test")
+{
+}
+
+}
