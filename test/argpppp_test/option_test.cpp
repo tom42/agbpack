ca7847a84f64fbe32f0633a6103f5403a777f8e7
tom <tom@z2>
2025-03-05 12:37:16 +0100

    Test option constructor

--- test/argpppp_test/option_test.cpp
+++ test/argpppp_test/option_test.cpp
@@ -8,9 +8,21 @@ import argpppp;
 namespace argpppp_test
 {
 
+using argpppp::of;
+using argpppp::option;
+
 TEST_CASE("option_test")
 {
-    FAIL("TODO: YIKES");
+    SECTION("constructor, all arguments given")
+    {
+        const auto opt = option("name", 'n', "arg", of::arg_optional, "doc", 123);
+        CHECK(opt.name() == "name");
+        CHECK(opt.key() == 'n');
+        CHECK(opt.arg() == "arg");
+        CHECK(opt.flags() == of::arg_optional);
+        CHECK(opt.doc() == "doc");
+        CHECK(opt.group() == 123);
+    }
 }
 
 }
