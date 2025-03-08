9baff22f417b9734b71c34303c118bf34bcb1dc8
tom <tom@z2>
2025-03-08 17:42:17 +0100

    Move conversion of options to argp_options into separate function

--- src/argpppp/parser.cpp
+++ src/argpppp/parser.cpp
@@ -3,6 +3,7 @@
 
 module;
 
+#include <algorithm>
 #include <argp.h>
 #include <optional>
 #include <string>
@@ -13,6 +14,23 @@ module argpppp;
 
 namespace argpppp
 {
+
+namespace
+{
+
+// TODO: here too: document that options must not go out of scope?
+// TODO: do we want to export and unit test this?
+// TODO: we could stick this into the option partition thing, no?
+std::vector<argp_option> to_argp_options(const std::vector<option>& options)
+{
+    std::vector<argp_option> argp_options;
+    argp_options.reserve(options.size() + 1);
+    std::transform(options.begin(), options.end(), back_inserter(argp_options), to_argp_option);
+    argp_options.push_back({});
+    return argp_options;
+}
+
+}
 
 void parser::add_option(const option& o)
 {
@@ -26,26 +44,16 @@ void parser::doc(const optional_string& s)
 
 void parser::parse(int argc, char** argv)
 {
-    // TODO: fill in all required fields
-    //       * options
+    // TODO: fill in all remaining fields
     //       * parser (callback function)
     //       * args_doc
-    //       * doc
     //       * children
     //       * help_filter
     //       * argp_domain
 
-    // TODO: turn option instances into argp_option structures. Can we unit test this? It IS testworthy, isn't it?
-    // TODO: do we build op this vector here, or as options are added
-    std::vector<argp_option> argp_options;
-    argp_options.reserve(m_options.size() + 1);
-    for (const auto& o : m_options)
-    {
-        argp_options.push_back(to_argp_option(o));
-    }
-    argp_options.push_back({});
+    const auto argp_options = to_argp_options(m_options);
+    const argp argp { argp_options.data(), nullptr, "TODO: args doc", c_str(m_doc), nullptr, nullptr, nullptr };
 
-    struct argp argp { argp_options.data(), nullptr, nullptr, c_str(m_doc), nullptr, nullptr, nullptr };
     argp_parse(&argp, argc, argv, 0, nullptr, this);
 }
 
