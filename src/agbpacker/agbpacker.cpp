76e42594df849d4047333527a6ab5121c260045a
tom <tom@z2>
2025-03-15 17:46:45 +0100

    Remove todo item: argpppp will keep its name. It's fine that way.

--- src/agbpacker/agbpacker.cpp
+++ src/agbpacker/agbpacker.cpp
@@ -39,7 +39,6 @@ int main(int argc, char** argv)
         //       Arguments
         //       * input file
         //       * output file
-        // TODO: re argpppp: while it initially seemed to be funny it looks totally hideous. Maybe rename it to just argpp
         argpppp::parser parser;
         parser.doc("Compress and decompress data for the GBA BIOS");
         parser.args_doc("arg1 arg2 ...");
