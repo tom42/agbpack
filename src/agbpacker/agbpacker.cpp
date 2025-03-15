8adecb4f57257157f956d9849aad2d324c6ff53f
tom <tom@z2>
2025-03-15 18:03:02 +0100

    Create options struct, factor out command line parsing

--- src/agbpacker/agbpacker.cpp
+++ src/agbpacker/agbpacker.cpp
@@ -14,31 +14,53 @@ extern "C"
 const char* argp_program_version = "agbpacker " AGBPACK_VERSION;
 }
 
+namespace
+{
+
+enum class mode
+{
+    compress,
+    decompress
+};
+
+class options final
+{
+public:
+    mode mode = mode::compress;
+};
+
+void parse_command_line(int argc, char** argv)
+{
+    // TODO: so what options do we need here?
+    //       -c => compress   \ These two override eachother, -c could be default
+    //       -d => decompress /
+    //       Some way to select the compression method
+    //       * LZSS, normal
+    //       * LZSS, vram safe
+    //       * 4 bit huffman
+    //       * 8 bit huffman
+    //       * RLE
+    //       * delta8
+    //       * delta16
+    //       * LZSS needs some more options: greedy/optimal/others
+    //       Arguments
+    //       * input file
+    //       * output file
+    argpppp::parser parser;
+    parser.doc("Compress and decompress data for the GBA BIOS");
+    parser.args_doc("<input> <output>");
+    add_option(parser, { "compress", 'c', {}, {}, "Files are compressed by default" }, [](auto){ return true; });
+    add_option(parser, { "decompress", 'd' }, [](auto){ return true; });
+    parser.parse(argc, argv);
+}
+
+}
+
 int main(int argc, char** argv)
 {
     try
     {
-        // TODO: so what options do we need here?
-        //       -c => compress   \ These two override eachother, -c could be default
-        //       -d => decompress /
-        //       Some way to select the compression method
-        //       * LZSS, normal
-        //       * LZSS, vram safe
-        //       * 4 bit huffman
-        //       * 8 bit huffman
-        //       * RLE
-        //       * delta8
-        //       * delta16
-        //       * LZSS needs some more options: greedy/optimal/others
-        //       Arguments
-        //       * input file
-        //       * output file
-        argpppp::parser parser;
-        parser.doc("Compress and decompress data for the GBA BIOS");
-        parser.args_doc("<input> <output>");
-        add_option(parser, { "compress", 'c', {}, {}, "Files are compressed by default" }, [](auto){ return true; });
-        add_option(parser, { "decompress", 'd' }, [](auto){ return true; });
-        parser.parse(argc, argv);
+        parse_command_line(argc, argv);
         return EXIT_SUCCESS;
     }
     catch (const std::exception& e)
