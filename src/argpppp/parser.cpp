c60d03605a7e0a627a651759200a35d672e15caa
tom42 <tom42@users.noreply.github.com>
2025-03-18 21:14:48 +0100

    parser: use get_names when formatting standard error message

--- src/argpppp/parser.cpp
+++ src/argpppp/parser.cpp
@@ -162,12 +162,8 @@ error_t parser::handle_option_callback_result(bool result, int key, char* arg, c
         //       * arg may be null
         //         * It may be optional and was not given (but is it then null or empty? Need to check with a real argp_parser I guess)
         //           * We may have a switch which never gets an argument. In this case we might want want to use a different message
-        //         * The option key may not be printable
-        //         * The option name may not be given
-        //         * Obviously we want a way to get the option name(s) as a string
-        //           * This is going to be tested
         // TODO: we really ought to have some hook to capture argp_failure output
-        argp_failure(state, EXIT_FAILURE, 0, "invalid argument '%s' for option '-%c'", arg, opt->key());
+        argp_failure(state, EXIT_FAILURE, 0, "invalid argument '%s' for option %s", arg, get_names(*opt).c_str());
         return EINVAL;
     }
 }
