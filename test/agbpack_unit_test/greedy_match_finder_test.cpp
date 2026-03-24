99fd1b63ef6ffcc696f06e14a808159034e34505
tom42 <tom42@users.noreply.github.com>
2026-03-24 07:26:20 +0100

    Workaround: MSVC chokes on our operator== in conjunction with catch2, so we parenthesize the asserted expressions for the time being.
    This makes the test work, at the expense of losing useful assertion
    messages.

--- test/agbpack_unit_test/greedy_match_finder_test.cpp
+++ test/agbpack_unit_test/greedy_match_finder_test.cpp
@@ -38,56 +38,54 @@ match find_match_vram(const std::string& data, std::size_t current_position)
 
 TEST_CASE("greedy_match_finder_test")
 {
-    // TODO: figure out why this crashes MSVC and fix it one way or another
-    /*
     SECTION("Empty input")
     {
-        CHECK(find_match_wram("", 0) == match(0, 0));
-        CHECK(find_match_wram("", 1) == match(0, 0));
+        CHECK((find_match_wram("", 0) == match(0, 0)));
+        CHECK((find_match_wram("", 1) == match(0, 0)));
     }
 
     SECTION("Literal bytes only")
     {
-        CHECK(find_match_wram("a", 0) == match(0, 0));
-        CHECK(find_match_wram("a", 1) == match(0, 0));
-        CHECK(find_match_wram("ab", 0) == match(0, 0));
-        CHECK(find_match_wram("ab", 1) == match(0, 0));
-        CHECK(find_match_wram("ab", 2) == match(0, 0));
-        CHECK(find_match_wram("abc", 0) == match(0, 0));
-        CHECK(find_match_wram("abc", 1) == match(0, 0));
-        CHECK(find_match_wram("abc", 2) == match(0, 0));
-        CHECK(find_match_wram("abc", 3) == match(0, 0));
+        CHECK((find_match_wram("a", 0) == match(0, 0)));
+        CHECK((find_match_wram("a", 1) == match(0, 0)));
+        CHECK((find_match_wram("ab", 0) == match(0, 0)));
+        CHECK((find_match_wram("ab", 1) == match(0, 0)));
+        CHECK((find_match_wram("ab", 2) == match(0, 0)));
+        CHECK((find_match_wram("abc", 0) == match(0, 0)));
+        CHECK((find_match_wram("abc", 1) == match(0, 0)));
+        CHECK((find_match_wram("abc", 2) == match(0, 0)));
+        CHECK((find_match_wram("abc", 3) == match(0, 0)));
     }
 
     SECTION("Reference of length 1")
     {
         // First 'a' is no match, second one is a match of length 1
-        CHECK(find_match_wram("aa", 0) == match(0, 0));
-        CHECK(find_match_wram("aa", 1) == match(1, 1));
+        CHECK((find_match_wram("aa", 0) == match(0, 0)));
+        CHECK((find_match_wram("aa", 1) == match(1, 1)));
 
         // Same, but a different character rather than EOF terminates the match
-        CHECK(find_match_wram("aab", 0) == match(0, 0));
-        CHECK(find_match_wram("aab", 1) == match(1, 1));
+        CHECK((find_match_wram("aab", 0) == match(0, 0)));
+        CHECK((find_match_wram("aab", 1) == match(1, 1)));
     }
 
     SECTION("Reference of length 2")
     {
         // First 'a' is no match, second one is a match of length 2, third one length 1 again
-        CHECK(find_match_wram("aaa", 0) == match(0, 0));
-        CHECK(find_match_wram("aaa", 1) == match(2, 1));
-        CHECK(find_match_wram("aaa", 2) == match(1, 2));
+        CHECK((find_match_wram("aaa", 0) == match(0, 0)));
+        CHECK((find_match_wram("aaa", 1) == match(2, 1)));
+        CHECK((find_match_wram("aaa", 2) == match(1, 2)));
 
         // Same, but a different character rather than EOF terminates the match
-        CHECK(find_match_wram("aaab", 0) == match(0, 0));
-        CHECK(find_match_wram("aaab", 1) == match(2, 1));
-        CHECK(find_match_wram("aaab", 2) == match(1, 2));
+        CHECK((find_match_wram("aaab", 0) == match(0, 0)));
+        CHECK((find_match_wram("aaab", 1) == match(2, 1)));
+        CHECK((find_match_wram("aaab", 2) == match(1, 2)));
     }
 
     SECTION("Reference of length 18 that overlaps with lookahead buffer")
     {
-        CHECK(find_match_wram("aaaaaaaaaaaaaaaaaaa", 0) == match(0, 0));
-        CHECK(find_match_wram("aaaaaaaaaaaaaaaaaaa", 1) == match(18, 1));
-        CHECK(find_match_wram("aaaaaaaaaaaaaaaaaaa", 2) == match(17, 2));
+        CHECK((find_match_wram("aaaaaaaaaaaaaaaaaaa", 0) == match(0, 0)));
+        CHECK((find_match_wram("aaaaaaaaaaaaaaaaaaa", 1) == match(18, 1)));
+        CHECK((find_match_wram("aaaaaaaaaaaaaaaaaaa", 2) == match(17, 2)));
     }
 
     SECTION("If there is more than one match the longer one is returned")
@@ -96,17 +94,16 @@ TEST_CASE("greedy_match_finder_test")
         // * A short one with length=3 and offset=5
         // * A long one with length=18 and offset=1
         // We want the longer one
-        CHECK(find_match_wram("aaabaaaaaaaaaaaaaaaaaaa", 5) == match(18, 1));
+        CHECK((find_match_wram("aaabaaaaaaaaaaaaaaaaaaa", 5) == match(18, 1)));
     }
 
     SECTION("VRAM safe encoding does not return matches with offset=1")
     {
         auto input = "aaaaaaaa";
-        CHECK(find_match_wram(input, 1) == match(7, 1));
-        CHECK(find_match_vram(input, 1) == match(0, 0));
-        CHECK(find_match_vram(input, 2) == match(6, 2));
+        CHECK((find_match_wram(input, 1) == match(7, 1)));
+        CHECK((find_match_vram(input, 1) == match(0, 0)));
+        CHECK((find_match_vram(input, 2) == match(6, 2)));
     }
-    */
 }
 
 }
--- test/agbpack_unit_test/huffman_encoder_tree_test.cpp
+++ test/agbpack_unit_test/huffman_encoder_tree_test.cpp
@@ -21,47 +21,45 @@ using agbpack_unit_testkit::create_tree_from_lucas_sequence;
 
 TEST_CASE("huffman_encoder_tree_test")
 {
-    // TODO: figure out why this crashes MSVC and fix it one way or another
-    /*
     SECTION("Create code table with maximum code length")
     {
         huffman_encoder_tree tree = create_tree_from_lucas_sequence(max_code_length + 1);
 
         auto code_table = tree.create_code_table();
 
-        CHECK(code_table[ 0] == code_table_entry( 0, 0b11111111111111111111111111111010, 32));
-        CHECK(code_table[ 1] == code_table_entry( 1, 0b11111111111111111111111111111011, 32));
-        CHECK(code_table[ 2] == code_table_entry( 2, 0b1111111111111111111111111111100, 31));
-        CHECK(code_table[ 3] == code_table_entry( 3, 0b111111111111111111111111111111, 30));
-        CHECK(code_table[ 4] == code_table_entry( 4, 0b11111111111111111111111111110, 29));
-        CHECK(code_table[ 5] == code_table_entry( 5, 0b1111111111111111111111111110, 28));
-        CHECK(code_table[ 6] == code_table_entry( 6, 0b111111111111111111111111110, 27));
-        CHECK(code_table[ 7] == code_table_entry( 7, 0b11111111111111111111111110, 26));
-        CHECK(code_table[ 8] == code_table_entry( 8, 0b1111111111111111111111110, 25));
-        CHECK(code_table[ 9] == code_table_entry( 9, 0b111111111111111111111110, 24));
-        CHECK(code_table[10] == code_table_entry(10, 0b11111111111111111111110, 23));
-        CHECK(code_table[11] == code_table_entry(11, 0b1111111111111111111110, 22));
-        CHECK(code_table[12] == code_table_entry(12, 0b111111111111111111110, 21));
-        CHECK(code_table[13] == code_table_entry(13, 0b11111111111111111110, 20));
-        CHECK(code_table[14] == code_table_entry(14, 0b1111111111111111110, 19));
-        CHECK(code_table[15] == code_table_entry(15, 0b111111111111111110, 18));
-        CHECK(code_table[16] == code_table_entry(16, 0b11111111111111110, 17));
-        CHECK(code_table[17] == code_table_entry(17, 0b1111111111111110, 16));
-        CHECK(code_table[18] == code_table_entry(18, 0b111111111111110, 15));
-        CHECK(code_table[19] == code_table_entry(19, 0b11111111111110, 14));
-        CHECK(code_table[20] == code_table_entry(20, 0b1111111111110, 13));
-        CHECK(code_table[21] == code_table_entry(21, 0b111111111110, 12));
-        CHECK(code_table[22] == code_table_entry(22, 0b11111111110, 11));
-        CHECK(code_table[23] == code_table_entry(23, 0b1111111110, 10));
-        CHECK(code_table[24] == code_table_entry(24, 0b111111110, 9));
-        CHECK(code_table[25] == code_table_entry(25, 0b11111110, 8));
-        CHECK(code_table[26] == code_table_entry(26, 0b1111110, 7));
-        CHECK(code_table[27] == code_table_entry(27, 0b111110, 6));
-        CHECK(code_table[28] == code_table_entry(28, 0b11110, 5));
-        CHECK(code_table[29] == code_table_entry(29, 0b1110, 4));
-        CHECK(code_table[30] == code_table_entry(30, 0b110, 3));
-        CHECK(code_table[31] == code_table_entry(31, 0b10, 2));
-        CHECK(code_table[32] == code_table_entry(32, 0b0, 1));
+        CHECK((code_table[ 0] == code_table_entry( 0, 0b11111111111111111111111111111010, 32)));
+        CHECK((code_table[ 1] == code_table_entry( 1, 0b11111111111111111111111111111011, 32)));
+        CHECK((code_table[ 2] == code_table_entry( 2, 0b1111111111111111111111111111100, 31)));
+        CHECK((code_table[ 3] == code_table_entry( 3, 0b111111111111111111111111111111, 30)));
+        CHECK((code_table[ 4] == code_table_entry( 4, 0b11111111111111111111111111110, 29)));
+        CHECK((code_table[ 5] == code_table_entry( 5, 0b1111111111111111111111111110, 28)));
+        CHECK((code_table[ 6] == code_table_entry( 6, 0b111111111111111111111111110, 27)));
+        CHECK((code_table[ 7] == code_table_entry( 7, 0b11111111111111111111111110, 26)));
+        CHECK((code_table[ 8] == code_table_entry( 8, 0b1111111111111111111111110, 25)));
+        CHECK((code_table[ 9] == code_table_entry( 9, 0b111111111111111111111110, 24)));
+        CHECK((code_table[10] == code_table_entry(10, 0b11111111111111111111110, 23)));
+        CHECK((code_table[11] == code_table_entry(11, 0b1111111111111111111110, 22)));
+        CHECK((code_table[12] == code_table_entry(12, 0b111111111111111111110, 21)));
+        CHECK((code_table[13] == code_table_entry(13, 0b11111111111111111110, 20)));
+        CHECK((code_table[14] == code_table_entry(14, 0b1111111111111111110, 19)));
+        CHECK((code_table[15] == code_table_entry(15, 0b111111111111111110, 18)));
+        CHECK((code_table[16] == code_table_entry(16, 0b11111111111111110, 17)));
+        CHECK((code_table[17] == code_table_entry(17, 0b1111111111111110, 16)));
+        CHECK((code_table[18] == code_table_entry(18, 0b111111111111110, 15)));
+        CHECK((code_table[19] == code_table_entry(19, 0b11111111111110, 14)));
+        CHECK((code_table[20] == code_table_entry(20, 0b1111111111110, 13)));
+        CHECK((code_table[21] == code_table_entry(21, 0b111111111110, 12)));
+        CHECK((code_table[22] == code_table_entry(22, 0b11111111110, 11)));
+        CHECK((code_table[23] == code_table_entry(23, 0b1111111110, 10)));
+        CHECK((code_table[24] == code_table_entry(24, 0b111111110, 9)));
+        CHECK((code_table[25] == code_table_entry(25, 0b11111110, 8)));
+        CHECK((code_table[26] == code_table_entry(26, 0b1111110, 7)));
+        CHECK((code_table[27] == code_table_entry(27, 0b111110, 6)));
+        CHECK((code_table[28] == code_table_entry(28, 0b11110, 5)));
+        CHECK((code_table[29] == code_table_entry(29, 0b1110, 4)));
+        CHECK((code_table[30] == code_table_entry(30, 0b110, 3)));
+        CHECK((code_table[31] == code_table_entry(31, 0b10, 2)));
+        CHECK((code_table[32] == code_table_entry(32, 0b0, 1)));
     }
 
     SECTION("Maximum code length exceeded")
@@ -72,7 +70,7 @@ TEST_CASE("huffman_encoder_tree_test")
             tree.create_code_table(),
             encode_exception,
             Catch::Matchers::Message("maximum code length exceeded"));
-    }*/
+    }
 }
 
 }
