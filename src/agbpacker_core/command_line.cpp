e13123ce55d9d41251d9ba4436b5eaaf55c9cefe
tom42 <tom42@users.noreply.github.com>
2026-04-07 07:48:29 +0200

    Add skeleton of command_line.cpp module

---
+++ src/agbpacker_core/command_line.cpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2026 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+module agbpacker_core;
+import argpppp;
+
+namespace agbpacker_core
+{
+
+parse_command_line_result parse_command_line(int /*argc*/, char* /*argv*/[])
+{
+	// TODO: call into argpppp
+	argpppp::options command_line_options;
+	return {};
+}
+
+}
