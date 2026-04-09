cb7201ee82001e520b6d4920879cf69ba423507d
tom42 <tom42@users.noreply.github.com>
2026-04-09 11:13:49 +0200

    Add note on how to specify compression mode [skip ci]

--- src/agbpacker_core/command_line.cpp
+++ src/agbpacker_core/command_line.cpp
@@ -34,6 +34,7 @@ parse_command_line_result parse_command_line(int argc, char* argv[], bool is_uni
     //       * 1 positional argument: <input>, output is optionally specified with option
     //           1a) If not given, output file name is same as input file name
     //           2a) If not given, output file name is somehow derived from input file name
+    // TODO: compression mode: consider making this an optional argument of -c/--compress?
     command_line_options
         .doc("Compress and decompress data for the GBA BIOS\nhttps://github.com/tom42/agbpack")
         .num_args(1);
