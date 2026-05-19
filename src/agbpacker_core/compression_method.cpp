11a470ddb7c28aece1c9d4b21d7025997d954a37
tom42 <tom42@users.noreply.github.com>
2026-05-19 07:56:45 +0200

    Add descriptors for all compression methods

--- src/agbpacker_core/compression_method.cpp
+++ src/agbpacker_core/compression_method.cpp
@@ -11,10 +11,15 @@ namespace agbpacker_core
 {
 
 // TODO: Accessor method, or do we simply declare it publicly?
-constexpr std::array<compression_method_info, 1> compression_methods =
+constexpr std::array<compression_method_info, 7> compression_methods =
 {
-	// TODO: add all here. How can we have constexpr strings in there? is that even possible?
-	compression_method_info(),
+    compression_method_info(compression_method::lzss, "lzss"),
+    compression_method_info(compression_method::optimal_lzss, "optimal_lzss"),
+    compression_method_info(compression_method::h4, "h4"),
+    compression_method_info(compression_method::h8, "h8"),
+    compression_method_info(compression_method::rle, "rle"),
+    compression_method_info(compression_method::d8, "d8"),
+    compression_method_info(compression_method::d16, "d16")
 };
 
 }
