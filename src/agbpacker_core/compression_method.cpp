81599675a29fe8cb1cf42345d8b7c7fde7432fe2
tom42 <tom42@users.noreply.github.com>
2026-05-20 08:13:16 +0200

    Fix for clang: need to use curly braces for aggregate initialization

--- src/agbpacker_core/compression_method.cpp
+++ src/agbpacker_core/compression_method.cpp
@@ -12,13 +12,13 @@ namespace agbpacker_core
 
 constexpr std::array<compression_method_info, 7> compression_methods =
 {
-    compression_method_info(compression_method::lzss, "lzss"),
-    compression_method_info(compression_method::optimal_lzss, "optimal_lzss"),
-    compression_method_info(compression_method::h4, "h4"),
-    compression_method_info(compression_method::h8, "h8"),
-    compression_method_info(compression_method::rle, "rle"),
-    compression_method_info(compression_method::d8, "d8"),
-    compression_method_info(compression_method::d16, "d16")
+    compression_method_info{ compression_method::lzss, "lzss" },
+    compression_method_info{ compression_method::optimal_lzss, "optimal_lzss" },
+    compression_method_info{ compression_method::h4, "h4" },
+    compression_method_info{ compression_method::h8, "h8" },
+    compression_method_info{ compression_method::rle, "rle" },
+    compression_method_info{ compression_method::d8, "d8" },
+    compression_method_info{ compression_method::d16, "d16" }
 };
 
 }
