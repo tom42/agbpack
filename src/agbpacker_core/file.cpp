eca1d719287b677ac8ba75b0b29c3c9e079b6ef2
tom42 <tom42@users.noreply.github.com>
2026-09-18 22:00:05 +0200

    Implement file::read_all_bytes()

--- src/agbpacker_core/file.cpp
+++ src/agbpacker_core/file.cpp
@@ -68,9 +68,15 @@ file file::open(const std::string& filename, const char* mode)
     return open(filename.c_str(), mode);
 }
 
-std::vector<unsigned char> file::read_all_bytes(const char* /*filename*/)
+std::vector<unsigned char> file::read_all_bytes(const char* filename)
 {
-    throw "TODO: implement";
+    auto file = open(filename, "rb");
+    std::size_t filesize = file.size();
+
+    std::vector<unsigned char> buffer(filesize);
+    file.read(buffer.data(), filesize);
+
+    return buffer;
 }
 
 std::vector<unsigned char> file::read_all_bytes(const std::string& filename)
