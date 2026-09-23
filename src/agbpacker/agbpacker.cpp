3b94e807dc183d1f8d6295616412ad529fa828cb
tom42 <tom42@users.noreply.github.com>
2026-09-24 00:00:09 +0200

    Remove obsolete todo (using compile time polymorphism with variant)

--- src/agbpacker/agbpacker.cpp
+++ src/agbpacker/agbpacker.cpp
@@ -52,8 +52,6 @@ void write_file(const std::string& filename, const bytevector& data)
 }
 
 // TODO: do not forget to add tests for the custom ctors we added
-// TODO: obviously we need to return something polymorphic here
-//       * Either define some sort of interface, or use a variant
 // TODO: configure the encoder where applicable (vram safety)
 // TODO: test whether vram_safe flag is applied:
 //       * lzss
